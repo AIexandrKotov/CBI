b0ea794f68bfe32cef4eb51b8db4da2b8b1fb9a9
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2022-03-19 17:39:48 +0300

    add bigintegers and sum

--- main.c
+++ main.c
@@ -1,6 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <memory.h>
 
-int main(int argc, char *argv[]) {
+struct biginteger_t
+{
+	unsigned char negative;
+	unsigned char count;
+	unsigned char* symbols;
+};
+
+typedef struct biginteger_t BigInteger;
+
+unsigned char digits_from_int(int a)
+{
+	if (a < 0) a = -a;
+	unsigned char ret = 0;
+	while (a > 0) a /= 10, ret++;
+	return ret;
+}
+
+void bi_init(BigInteger* p)
+{
+	if (p->count < 1) exit(EXIT_FAILURE);
+	p->symbols = malloc(p->count);
+}
+
+void bi_free(BigInteger* p)
+{
+	free(p->symbols);
+	free(p);
+}
+
+BigInteger* bi_from_int(int src)
+{
+	BigInteger* ret = calloc(1, sizeof(BigInteger));
+	if (ret == NULL) exit(EXIT_FAILURE);
+	ret->negative = src < 0;
+	ret->count = digits_from_int(src);
+	bi_init(ret);
+	
+	if (src < 0) src = -src;
+	for (unsigned char i = 0; i < ret->count; i++)
+	{
+		*(ret -> symbols + i) = src % 10;
+		src /= 10;
+	}
+	return ret;
+}
+
+void bi_print(BigInteger* p)
+{
+	if (p->negative) printf("-");
+	for (int i = p->count - 1; i >= 0; i--)
+		printf("%d", *(p->symbols + i));
+}
+
+void internal_bi_sum(BigInteger* left, BigInteger* right, BigInteger* ret)
+{
+	unsigned char context = 0;
+	unsigned char sum;
+	for (unsigned char i = 0; i < left->count; i++)
+	{
+		sum = context;
+		sum += *(left->symbols + i) + *(right -> symbols + i);
+		context = sum / 10;
+		*(ret -> symbols + i) = sum % 10;
+	}
+	*(ret->symbols + ret->count - 1) = context;
+}
+
+BigInteger* bi_sum(BigInteger* left, BigInteger* right)
+{
+	BigInteger* ret = calloc(1, sizeof(BigInteger));
+	ret->negative = left->negative ^ right->negative;
+	if (left->count == right->count) ret->count = left->count + 1;
+	else ret->count = left->count > right->count ? left->count + 1 : right->count + 1;
+	ret->symbols = malloc(ret->count);
+	internal_bi_sum(left, right, ret);
+	if (*(ret->symbols + ret->count - 1) == 0) realloc(ret, ret->count = ret->count - 1);
+	return ret;
+}
+
+int main(int argc, char *argv[])
+{
+	BigInteger* bi1 = bi_from_int(INT_MAX);
+	BigInteger* bi2 = bi_from_int(INT_MAX);
+	
+	bi_print(bi1);
+	printf("\n");
+	bi_print(bi2);
+	printf("\n");
+	
+	BigInteger* bi3 = bi_sum(bi1, bi2);
+	bi_print(bi3);
+	
+	bi_free(bi1);
+	bi_free(bi2);
+	bi_free(bi3);
 	return 0;
 }
